0fbfa6aeb18abae6fbecb183532644968e31fb1e
CharlieM044 <charliemason044@gmail.com>
2026-05-26 17:29:35 +1000

    dgf

--- robo code final/rasberry pi/state.py
+++ robo code final/rasberry pi/state.py
@@ -6,7 +6,7 @@ Import this anywhere instead of passing dicts around.
 import time
 
 state = {
-    'mode':       'autonomous',   # 'autonomous' | 'manual'
+    'mode':       'manual',        # 'autonomous' | 'manual'
     'running':    True,
     'streaming':  False,
     'start_time': time.time(),
